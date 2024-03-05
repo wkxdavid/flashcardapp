97d879bddebbf4e90079735107825bd6ad1f81b0
Scott Nguyen <scottn523@gmail.com>
2024-03-04 20:11:10 -0800

    attached firebase

--- app/src/main/java/edu/uw/ischool/scottng/memorymentor/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/scottng/memorymentor/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.uw.ischool.scottng.memorymentor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
