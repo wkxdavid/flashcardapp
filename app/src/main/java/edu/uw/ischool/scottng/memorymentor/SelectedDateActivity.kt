8049433f0813c308093498603ddff3f35c79bdcc
nonzinv <nonzinv@uw.edu>
2024-03-09 18:15:20 -0800

    added more things

--- app/src/main/java/edu/uw/ischool/scottng/memorymentor/SelectedDateActivity.kt
+++ app/src/main/java/edu/uw/ischool/scottng/memorymentor/SelectedDateActivity.kt
@@ -1,10 +1,12 @@
 package edu.uw.ischool.scottng.memorymentor
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 
 class SelectedDateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,18 @@ class SelectedDateActivity : AppCompatActivity() {
         setContentView(R.layout.activity_selected_date)
         val dateTextView = findViewById<TextView>(R.id.dateTextView)
         val noteEditText = findViewById<EditText>(R.id.noteEditText)
-        val intent = intent
-        if (intent != null && intent.hasExtra("selectedDate")) {
-            val selectedDate = intent.getStringExtra("selectedDate")
-            dateTextView.text = selectedDate
-        }
+        val scheduleButton = findViewById<Button>(R.id.scheduleButton)
+        val selectedDate = intent.getStringExtra("selectedDate")
+        dateTextView.text = selectedDate
+        scheduleButton.setOnClickListener(View.OnClickListener { // Get the event note
+            val note = noteEditText.text.toString().trim { it <= ' ' }
+            if (note.isEmpty()) {
+                Toast.makeText(this@SelectedDateActivity, "Enter Exam Name", Toast.LENGTH_SHORT)
+                    .show()
+                return@OnClickListener
+            }
+            Toast.makeText(this@SelectedDateActivity, "Event scheduled: $note", Toast.LENGTH_SHORT)
+                .show()
+        })
     }
 }
