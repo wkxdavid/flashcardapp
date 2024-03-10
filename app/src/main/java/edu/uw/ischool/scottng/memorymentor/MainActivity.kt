69a2a24920e181cee749f9722f0e32d4ae24e292
wkxdavid <dpham722@uw.edu>
2024-03-10 14:13:00 -0700

    working log in and authenication

--- app/src/main/java/edu/uw/ischool/scottng/memorymentor/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/scottng/memorymentor/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
+import android.content.Intent
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +18,23 @@ class MainActivity : AppCompatActivity() {
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        myRef.setValue("Hello, World TEST!")
+
+        auth = FirebaseAuth.getInstance()
+
+        findViewById<Button>(R.id.registerButton).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
